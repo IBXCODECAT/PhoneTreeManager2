1503a04dc988f0f015257b9260a2573b4aacbcbe
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2024-12-07 21:56:22 -0600

    Ugh

--- app/src/main/java/com/example/phonetreemanager/ui/call_info/CallInfoFragment.kt
+++ app/src/main/java/com/example/phonetreemanager/ui/call_info/CallInfoFragment.kt
@@ -1,10 +1,14 @@
 package com.example.phonetreemanager.ui.call_info
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import com.example.phonetreemanager.databinding.FragmentCallinfoBinding
 import com.example.phonetreemanager.ui.home.HomeViewModel
@@ -29,6 +33,15 @@ class CallInfoFragment : Fragment() {
         _binding = FragmentCallinfoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val btnSubmit: Button = binding.btnSubmit;
+
+        btnSubmit.setOnClickListener(View.OnClickListener {
+            val manager: FragmentManager = requireActivity().supportFragmentManager
+            manager.popBackStack()
+            
+            // Toast notification
+            Toast.makeText(this.context, "Phone Calls Updated!", Toast.LENGTH_SHORT).show()
+        })
 
         return root
     }
